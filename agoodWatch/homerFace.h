f56177bdd0d7767a01d8ed7b33c5b5f0e5c2484a
Guilhem Bonnefille <guilhem.bonnefille@gmail.com>
2021-01-26 23:06:59 +0100

    Extract Homer face

---
+++ agoodWatch/homerFace.h
@@ -0,0 +1,17 @@
+/*
+ * Copyright (c) 2020 Alex Goodyear
+ * Derived from ...
+Copyright (c) 2019 lewis he
+This is just a demonstration. Most of the functions are not implemented.
+The main implementation is low-power standby. 
+The off-screen standby (not deep sleep) current is about 4mA.
+Select standard motherboard and standard backplane for testing.
+Created by Lewis he on October 10, 2019.
+*/
+
+#ifndef __HOMER_FACE_H
+#define __HOMER_FACE_H
+
+int createHomerFace (int);
+
+#endif /*__HOMER_FACE_H */
